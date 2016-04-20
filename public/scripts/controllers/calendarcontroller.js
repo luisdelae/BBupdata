f36b553e7b7789ef47c521b6a99322df2e6b56ac
Luis De La Espriella <luisdelae@gmail.com>
2016-04-19 21:49:53 -0500

    changed calendar to show both first and last names on reminders

--- public/scripts/controllers/calendarcontroller.js
+++ public/scripts/controllers/calendarcontroller.js
@@ -32,7 +32,7 @@ myApp.controller('CalendarController', ['$scope', '$http', 'ContactFactory',
       console.log('userReminders from controller: ', $scope.userReminders);
       //return an object with an array of objects
       for(var i = 0; i < $scope.userReminders.length; i++) {
-        $scope.events.push({title: (($scope.userReminders[i].name))[0] +
+        $scope.events.push({title: $scope.userReminders[i].name +
         ': ' + $scope.userReminders[i].subject, start: moment.tz($scope.userReminders[i].date,
           moment.tz.guess()), stick: true, backgroundColor: changeBgColor(i)});
       }
